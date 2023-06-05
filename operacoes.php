947a7a8fb24f04e35678ec9d8e8adbe049aff9f9
Clécio Santos <91676554+cleciocsantos@users.noreply.github.com>
2023-06-05 14:37:29 -0300

    Solução da ATV2

--- operacoes.php
+++ operacoes.php
@@ -8,4 +8,35 @@
         $valor = 2 * $valor;
         return $valor;
     }
+
+    function preco_inferior($precos, $valor){
+        $quantidade = 0;
+        foreach($precos as $preco){
+            if($preco < $valor){
+                $quantidade++;
+            }
+        }
+        return $quantidade;
+    }
+
+    function preco_entre($nomes, $precos, $minimo, $maximo){
+        for($i=0; $i < sizeof($precos); $i++){
+            if($precos[$i] >= $minimo and $precos[$i] <= $maximo){
+                echo $nomes[$i]. "<br>";
+            }
+        }
+    }
+
+    function preco_superior($precos, $valor){
+        $quantidade = 0;
+        $soma = 0;
+        foreach($precos as $preco){
+            if($preco > $valor){
+                $quantidade++;
+                $soma += $preco;
+            }
+        }
+        $media = $soma/$quantidade;
+        return $media;
+    }
 ?>
--- teste.php
+++ teste.php
@@ -5,12 +5,31 @@
     </head>
     <body>
         <?php
-            $n1 = 2;
+            /*$n1 = 2;
             $n2 = 3;
             $resp1 = soma_valores($n1, $n2);
             $resp2 = dobro($n2);
             echo "A soma de $n1 e $n2 é $resp1.<br>";
-            echo "O dobro de $n2 é $resp2.<br>";
+            echo "O dobro de $n2 é $resp2.<br>";*/
+
+            echo "<br>ATV2 -- item a <br>";
+            $precos = array(5, 60, 120, 40, 50, 130);
+            $valor = 45;
+            $quantidade = preco_inferior($precos, $valor);
+            echo $quantidade;
+
+            echo "<br>ATV2 -- item b <br>";
+            $nomes = array("caneta", "caderno", "teclado", "fone", "mochila", "mouse");
+            $precos = array(5, 60, 120, 40, 50, 130);
+            $minimo = 50;
+            $maximo = 100;
+            preco_entre($nomes, $precos, $minimo, $maximo);
+
+            echo "<br>ATV2 -- desafio extra <br>";
+            $precos = array(5, 60, 120, 40, 50, 130);
+            $valor = 100;
+            $media = preco_superior($precos, $valor);
+            echo $media;
         ?>
     </body>
 </html>
